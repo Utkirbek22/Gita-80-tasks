cd48b7e3572638d5e2af2d156f4d0c24c7ed5cd5
Utkirbek22 <Socialmedia05112022@gmail.com>
2026-04-27 21:17:47 +0200

    task 14 is done

---
+++ task14.py
@@ -0,0 +1,18 @@
+nums = [1,2,3,53,4,4,5,5,24,53,1,34]
+max_value = nums[0]
+max_index = 0
+
+min_value = nums[0]
+min_index = 0
+for i in range(len(nums)):
+    if nums[i] >= max_value:
+        max_value = nums[i]
+        max_index = i
+    if nums[i] <= min_value:
+        min_value = nums[i]
+        min_index = i
+
+
+
+print(max_value, max_index)
+print(min_value,min_index,)
