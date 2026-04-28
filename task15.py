c3ff212783f55492556364001793b0ecae10e27d
Utkirbek22 <Socialmedia05112022@gmail.com>
2026-04-28 17:53:34 +0200

    findig the firs occurance min or max value, task 15

---
+++ task15.py
@@ -0,0 +1,22 @@
+nums = [8,2,3,53,4,4,5,5,24,53,34]
+max_value = nums[0]
+max_index = 0
+
+min_value = nums[0]
+min_index = 0
+for i in range(len(nums)):
+    if nums[i] > max_value:
+        max_value = nums[i]
+        max_index = i
+    if nums[i] < min_value:
+        min_value = nums[i]
+        min_index = i
+if min_index < max_index:
+    print(min_value,min_index)
+else:
+    print(max_value,max_index)
+
+
+
+# print(max_value, max_index)
+# print(min_value,min_index,)
