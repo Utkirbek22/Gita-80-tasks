d2ca00ee90092a5b6dec3d37f422cd5ef20021a5
Utkirbek22 <Socialmedia05112022@gmail.com>
2026-05-12 13:45:26 +0200

    task 50 is done also perfectly

---
+++ task50.py
@@ -0,0 +1,16 @@
+m,n = map(int, input("Enter row and columns: ").split())
+matrix = []
+for i in range(m):
+    row = list(map(int, input("Enter the number: ").split()))
+    matrix.append(row)
+for i in range(m):
+    print(*matrix[i])
+last_row = []
+for i in range(n):
+    max_value = matrix[0][i]
+    for j in range(m):
+        if matrix[j][i] > max_value:
+            max_value = matrix[j][i]
+    last_row.append(max_value)
+print(*last_row)
+
