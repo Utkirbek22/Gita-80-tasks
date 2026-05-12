2cfec633e58dbc1c330cd17d87b5c94a60961cf2
Utkirbek22 <Socialmedia05112022@gmail.com>
2026-05-12 20:05:53 +0200

    task 53 is done, matrix class has done, it was great lesson, i learnt a lot and i do enjoyed them

---
+++ task53.py
@@ -0,0 +1,20 @@
+m,n = map(int, input("Enter col and column: ").split())
+matrix = []
+
+for i in range(m):
+    row = list(map(int, input("Enter number: ").split()))
+    matrix.append(row)
+min_value = matrix[0][0]
+row = 0
+col = 0
+for i in range(m):
+    for j in range(n):
+        if matrix[i][j] < min_value:
+            min_value = matrix[i][j]
+            row = i
+            col = j
+matrix.pop(row)
+
+for i in range(len(matrix)):
+    print(*matrix[i])
+
